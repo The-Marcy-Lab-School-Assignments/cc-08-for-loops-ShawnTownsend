583a754631dee02d609c911e6bc6642e20621ac7
ShawnTownsend <stitans28@gmail.com>
2021-10-26 14:11:28 +0000

    Completed the questions

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
 //1
-function countToTwenty(){
-  
+function countToTwenty(i){
+  for (let i = 1; i <= 20; i++); {
+  console.log(i);
+  }
 }
 
 //2
-function countFromTwenty(){
-  
+function countFromTwenty(i){
+  for (let i = 20; i >= 1; i--); {
+  console.log(i);
+  }
 }
 
 //3
-function countToN(){
-  
+function countToN(n){
+  for (let i = 1; i <= n; i++); {
+  console.log(n);
+  }
 }
 
 
 //4
-function countFromN(){
-  
+function countFromN(n){
+  for (let i = n; i >= 1; i--); {
+  console.log(n);
+  }
 }
 
 //5
-function countEveryOdd(){
-  
+function countEveryOdd(n){
+     for(let i = 1; i <= n; i++){
+        if(i % 2 !=0)     
+        console.log(i);
+}
 }
